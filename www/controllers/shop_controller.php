335d5c767a648612a38ce6dea2a790ba72e7ba8f
Denys Samoilenko <denniksam@gmail.com>
2022-12-19 21:03:01 +0200

    final

--- www/controllers/shop_controller.php
+++ www/controllers/shop_controller.php
@@ -106,7 +106,8 @@ else if( $_SERVER[ 'REQUEST_METHOD' ] == 'GET' ) {
     // echo $where_part; exit ;
     }
     else {
-        // фильтры
+        //////////////////////////////// примененные фильтры //////////////////////////////////////////
+        // макс-мин цены
         $filters = [] ;
         if( isset( $_GET['minprice'] ) && is_numeric( $_GET['minprice'] ) ) {   // фильтр на мин. цену
             $where_part = " WHERE p.price >= {$_GET['minprice']}" ;
@@ -117,11 +118,26 @@ else if( $_SERVER[ 'REQUEST_METHOD' ] == 'GET' ) {
                 ( ($where_part == "") ? " WHERE " : " AND " )
                 . " p.price <= {$_GET['maxprice']}" ;
             $filters[ 'maxprice' ] = $_GET[ 'maxprice' ] ;
+        }       
+        // группы товаров - по принципу id=grp -- ищем параметры со значением grp и берем их имена
+        $filters[ 'product_groups_id' ] = [] ;
+        $filters[ 'product_groups_name' ] = [] ;
+        foreach( $_GET as $k => $v ) {
+            if( $v == 'grp' ) {
+                $filters[ 'product_groups_id' ][] = $k ;
+            }
         }
+        if( count( $filters[ 'product_groups_id' ] ) > 0 ) {  // AND p.id_grp IN ( '111','222','333' )
+            $where_part .=  
+                ( ($where_part == "") ? " WHERE " : " AND " )
+                . "p.id_grp IN ( '" . implode( "','", $filters[ 'product_groups_id' ] ) . "' ) " ;
+        }
+
         $view_data[ 'filters' ] = $filters ;
     }
 
     // echo $where_part ; exit ;
+    /////////////////////////////////////////  ДАННЫЕ ДЛЯ ФИЛЬТРОВ /////////////////////////////////
     // макс и мин цены
     $sql = "SELECT MIN(p.price), MAX(p.price) FROM Products p" ;
     try { 
@@ -133,10 +149,25 @@ else if( $_SERVER[ 'REQUEST_METHOD' ] == 'GET' ) {
         $_CONTEXT['logger']( 'shop_controller3 ' . $ex->getMessage() . $sql ) ;
         $view_data[ 'add_error' ] = "Server error try later" ;
     }
+    // Категории (группы) товаров
+    $sql = "SELECT g.id, MAX(g.name) AS name, COUNT(p.id) AS cnt FROM `product_groups` g JOIN products p ON g.id=p.id_grp GROUP BY 1" ;
+    try { 
+        $table = $_CONTEXT[ 'connection' ]->query( $sql ) ;
+        $view_data[ 'product_groups' ] = [] ;
+        while( $row = $table->fetch( PDO::FETCH_ASSOC ) ) {
+            $view_data[ 'product_groups' ][] = $row ;
+            if( in_array( $row['id'], $filters[ 'product_groups_id' ] ) )  {
+                $filters[ 'product_groups_name' ][] = $row['name'] ;  // имена выбранных групп для отображения фильтра
+            }
+        }
+    }
+    catch( PDOException $ex ) {
+        $_CONTEXT['logger']( 'shop_controller4 ' . $ex->getMessage() . $sql ) ;
+        $view_data[ 'add_error' ] = "Server error try later" ;
+    }
 
 
-
-    // пагинация
+    ///////////////////////////////////////////// пагинация //////////////////////////////////////////
     // 1. сколько всего товаров
     $sql = "SELECT COUNT(p.id) FROM Products p  $where_part " ;
     try { $total = $_CONTEXT[ 'connection' ]->query( $sql )->fetch(PDO::FETCH_NUM)[0] ; }
@@ -148,6 +179,7 @@ else if( $_SERVER[ 'REQUEST_METHOD' ] == 'GET' ) {
         // 2. номер страницы и кол-во элементов на странице
         $perpage = 4 ;
         $lastpage = ceil( $total / $perpage ) ;
+        if( $lastpage == 0 ) $lastpage = 1 ;
         @$page = intval( $_GET['page'] ) ?? 1 ;         //  $page  1      2      3      4       
         if( $page < 1 ) $page = 1 ;                     //  nums   1-4    5-8    9-12   13,14,15 (всего 15)
         if( $page > $lastpage ) $page = $lastpage ;     //  $skip  0      4      8      12
@@ -160,7 +192,7 @@ else if( $_SERVER[ 'REQUEST_METHOD' ] == 'GET' ) {
         ] ;
  // echo $total, ' ', $_GET['page'], '<br/>' ; print_r( $view_data[ 'paginator' ] ) ; exit ; 
 
-        $sql = "SELECT * FROM Products p   $where_part   $order_part     LIMIT $skip, $perpage" ;
+        $sql = "SELECT p.*, g.name as `grp_name` FROM Products p JOIN product_groups g ON p.id_grp = g.id   $where_part   $order_part     LIMIT $skip, $perpage" ;
         try {
             $view_data[ 'products' ] = 
                 $_CONTEXT[ 'connection' ]->query( $sql )->fetchAll( PDO::FETCH_ASSOC ) ;
@@ -188,10 +220,29 @@ CREATE TABLE product_groups (
 
 INSERT INTO product_groups(name) VALUES ('Программное обеспечение');
 
+Таблица типа "журнал" - постоянно изменяемая таблица
+В таблицах типа "словарь" (с почти неизменными данными) id можно вибирать не UUID, а по семантике
+CREATE TABLE user_roles (  
+    id VARCHAR(16) PRIMARY KEY,
+    descr TINYTEXT NOT NULL
+) ENGINE = InnoDB, DEFAULT CHARSET = UTF8 ;
+INSERT INTO user_roles VALUES( 'admin', 'Системный администратор - доступ к DDL' )
+INSERT INTO user_roles VALUES( 'moderator', 'Контент-модератор - доступ к DML' )
+INSERT INTO user_roles VALUES( 'guest', 'Гость - минимальные права на просмотр открытого контента' )
+
+ALTER TABLE users ADD COLUMN role_id VARCHAR(16) NOT NULL DEFAULT 'guest'
+ALTER TABLE users ADD FOREIGN KEY (role_id) REFERENCES user_roles (id)
+
 Д.З. Создать таблицу product_groups ( 
     id CHAR(36) PRIMARY KEY  DEFAULT UUID(),
     name TINYTEXT NOT NULL
 ) ENGINE = InnoDB, DEFAULT CHARSET = UTF8 ;
 Заполнить ее несколькими категориями (хотя бы 3) - для будущего фильтра
 Для товаров (таблица products) указать id группы 
+
+Д.З. (Экзаменационное): Приложить архив итогового проекта
+Доработать: фильтр по группам товаров
+Ожидаемое поведение:
+ - если  выбраны все группы, то сбросить фильтр групп (как будто нет выбранных вообще)
+ - также учитывать этот факт в пагинаторе
 */
--- www/views/shop.php
+++ www/views/shop.php
@@ -11,14 +11,27 @@
     <h4>Фильтры:</h4>
     Цена: от <input type=number name=minprice value=<?= $view_data['minprice'] ?> min=<?= $view_data['minprice'] ?>  max=<?= $view_data['maxprice'] ?> /> 
           до <input type=number name=maxprice value=<?= $view_data['maxprice'] ?> min=<?= $view_data['minprice'] ?>  max=<?= $view_data['maxprice'] ?> /><br/>
+    <!-- ********************** Группы товаров ********************************** -->
+    <?php foreach( $view_data[ 'product_groups' ] as $grp ) : ?>
+       <label>
+        <input  type="checkbox" 
+                name="<?= $grp['id'] ?>" 
+                value="grp" 
+                <?= ( in_array( $grp['id'], $filters[ 'product_groups_id' ] ) ) ? 'checked' : '' ?> 
+                /> 
+        <?= $grp['name'] ?> (<?= $grp['cnt'] ?>) 
+    </label><br/>
+    <?php endforeach ?>
+    <br/>
     <button>Применить фильтры</button>
 </form>
-
+<br/>
 <form>
     Поиск: <input name=search /> <button>найти</button>
 </form>
 
 <h3>Всего <?= $view_data[ 'paginator' ][ 'total' ] ?> позиций </h3>
+<!-- ************************************* Примененные фильтры ******************************************* -->
 <h4>
     <?php if( isset( $view_data[ 'filters' ][ 'minprice' ] ) ) : ?>
         Цена от  <?= $view_data[ 'filters' ][ 'minprice' ] ?> <br/>
@@ -29,7 +42,10 @@
     <?php if( isset( $view_data[ 'search' ] ) ) : ?>
         Результат поиска " <?= $view_data[ 'search' ] ?> " <br/>
     <?php endif ?>
-    <a href="/shop">Сбросить все фильтры</a>
+    <?php if( ! empty( $filters[ 'product_groups_name' ] ) ) : ?>
+        Группы товаров: <?= implode( ', ', $filters[ 'product_groups_name' ] ) ?> <br/>
+    <?php endif ?>
+    <br/><a href="/shop">Сбросить все фильтры</a>
 </h4>
 <?php if( empty( $view_data[ 'products' ] ) ) : ?>
     <p>
@@ -40,7 +56,7 @@
     <div class="img-container" >
         <img src="/images/<?= $product['image'] ?>" />
     </div>
-    <h4><?= $product['name'] ?></h4>
+    <h4><?= $product['name'] ?> (<i><?= $product['grp_name'] ?></i>)</h4>
     <h5><?= $product['descr'] ?></h5>
     <b><?= $product['price'] ?></b>
     <?php if( ! empty( $product['discount'] ) ) : ?>
@@ -76,6 +92,9 @@
         . ( ( isset( $view_data[ 'search' ] ) ) 
                 ? "search=" . $view_data[ 'search' ] . "&"
                 : "" ) 
+        . ( ( ! empty( $filters[ 'product_groups_id' ] ) ) 
+                ? implode( '=grp&', $filters[ 'product_groups_id' ] ) . '=grp&'
+                : "" )  
         ;
 ?>
 <div class='paginator'>
@@ -100,7 +119,7 @@
     <?php endif ?>
 </div>
 
-
+<?php if( ! empty( $_CONTEXT[ 'auth_user' ] ) and $_CONTEXT[ 'auth_user' ][ 'role_id' ] == 'admin' ) : ?>
 
 <form method="post" enctype="multipart/form-data" >
     <input type="text"   name="name"     placeholder="Название" /><br/>
@@ -112,6 +131,9 @@
 </form>
 
 <?= $view_data[ 'add_error' ] ?? ''  ?>
+
+<?php endif ?>
+
 
 <?php
 
